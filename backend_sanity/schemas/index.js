68e3cf6c94c44776b82c9e55eb96c96ab4076e5e
LUCAS-TCHITDJIAN <tchitdjian.lucas@gmail.com>
2022-12-19 16:08:16 +0100

    NavBar Component finished

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,9 @@
+import testimonials from "./testimonials";
+import works from './works';
+import brands from './brands';
+import about from './about';
+import contact from './contact';
+import skills from './skills';
+import workExperience from './workExperience';
+
+export const schemaTypes = [testimonials, works, workExperience, brands, about, contact, skills]
